ad8c72162c69903e132c5dbcfc6d5f5f4b3df6ef
pransu-coder <pransu145@gmail.com>
2025-12-16 16:53:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config(); // local ke liye, Render ignore karta hai
+dotenv.config();
 
 import express from "express";
+import cors from "cors";
 import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use(express.static("public"));
 
 app.use("/api/chat", chatRoutes);
 
@@ -15,9 +16,8 @@ app.get("/health", (req, res) => {
   res.json({ status: "ok" });
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
   console.log("GROQ KEY FOUND:", !!process.env.GROQ_API_KEY);
 });
-
